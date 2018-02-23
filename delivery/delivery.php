9be43e5c03d1d651d6f439f55b5d95cc0c279e73
creaivecoder567 <sarathcreativethinker@gmail.com>
2018-02-23 19:32:51 +0530

    delivery is modified.

--- delivery/delivery.php
+++ delivery/delivery.php
@@ -9,13 +9,13 @@ $response["litre:"]=array();
 $json = array();
 
 if (array_key_exists('dboyid', $data)){
-    $DBOYID= $data['dboyid'];
-  }
+  $DBOYID= $data['dboyid'];
+}
 
 
 //echo "Today is " . date("d-m-Y") . "<br>";
 $today=date("d-m-Y");
-$today="18-01-2018";
+//$today="18-01-2018";
 //$sql="select date from litre WHERE date ='$date' AND  userid= (select user_id from order_history where location = 'Banshankari')";
 date_default_timezone_set('Asia/Calcutta');
 $ctime=date('H:i:s');
@@ -25,34 +25,34 @@ if( strtotime($ctime)<=strtotime('12:00:00') )
 
   switch ($DBOYID) {
     case 'SVD_001':
-      $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'HSR Layout')";
-      break;
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'HSR Layout')";
+    break;
 
-      case 'SVD_002':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Bannerghatta')";
-        break;
+    case 'SVD_002':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Bannerghatta')";
+    break;
 
-        case 'SVD_003':
-          $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location IN ('Bilekahalli'))";
-          break;
-            case 'SVD_004':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Begur')";
-        break;
-        case 'SVD_005':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'JP Nagar')";
-        break;
-         case 'SVD_006':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Dodda Kammanahalli)";
-        break;
-         case 'SVD_007':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Jaya Nagar')";
-        break;
-        case 'SVD_008':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'BTM Layout')";
-        break;
+    case 'SVD_003':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location IN ('Bilekahalli'))";
+    break;
+    case 'SVD_004':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Begur')";
+    break;
+    case 'SVD_005':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'JP Nagar')";
+    break;
+    case 'SVD_006':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Dodda Kammanahalli)";
+    break;
+    case 'SVD_007':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'Jaya Nagar')";
+    break;
+    case 'SVD_008':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND active_m='0' AND orderid  IN (select id from order_history where location = 'BTM Layout')";
+    break;
     default:
-      # code...
-      break;
+    # code...
+    break;
   }
 
   $result=mysqli_query($db,$sql);
@@ -63,73 +63,73 @@ if( strtotime($ctime)<=strtotime('12:00:00') )
   $final["user"]=array();
 
 
-   while ($row=mysqli_fetch_assoc($result)) {
+  while ($row=mysqli_fetch_assoc($result)) {
 
-  $orderid=$row['orderid'];
-  $sql2="select * from order_history where id= $orderid";
-  $result2=mysqli_query($db,$sql2);
+    $orderid=$row['orderid'];
+    $sql2="select * from order_history where id= $orderid";
+    $result2=mysqli_query($db,$sql2);
 
-  while ($row2=mysqli_fetch_assoc($result2)) {
-  $address= $row2['address'];
-  }
+    while ($row2=mysqli_fetch_assoc($result2)) {
+      $address= $row2['address'];
+    }
 
     $id= $row['userid'];
-     $sql1="select * from user where id= $id ";
+    $sql1="select * from user where id= $id ";
     $result1=mysqli_query($db,$sql1);
-  if ($row['morning_litre']!='0') {
-    while($row1=mysqli_fetch_assoc($result1)){
-       $user=array(
-         "userid" => $row['userid'],
-         "name"=>$row1['name'],
-         "mobile"=>$row1['mobile'],
-         "orderid" => $row['orderid'],
-         "dateid" => $row['id'],
-         "morning_litre" => $row['morning_litre'],
-         "address"=>$address
-       );
-       array_push($final["user"], $user);
+    if ($row['morning_litre']!='0') {
+      while($row1=mysqli_fetch_assoc($result1)){
+        $user=array(
+          "userid" => $row['userid'],
+          "name"=>$row1['name'],
+          "mobile"=>$row1['mobile'],
+          "orderid" => $row['orderid'],
+          "dateid" => $row['id'],
+          "morning_litre" => $row['morning_litre'],
+          "address"=>$address
+        );
+        array_push($final["user"], $user);
+      }
     }
+
+
+
+
   }
-
-
-
-
-   }
 }
 else
 {
 
   switch ($DBOYID) {
 
-         case 'SVD_001':
-      $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'HSR Layout')";
-      break;
+    case 'SVD_001':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'HSR Layout')";
+    break;
 
-      case 'SVD_002':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Bannerghatta')";
-        break;
+    case 'SVD_002':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Bannerghatta')";
+    break;
 
-        case 'SVD_003':
-          $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location IN ('HSRLayout','Bilekahalli'))";
-          break;
-            case 'SVD_004':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Begur')";
-        break;
-        case 'SVD_005':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'JP Nagar')";
-        break;
-         case 'SVD_006':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Dodda Kammanahalli)";
-        break;
-         case 'SVD_007':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Jaya Nagar')";
-        break;
-        case 'SVD_008':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'BTM Layout')";
-        break;
+    case 'SVD_003':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location IN ('Bilekahalli'))";
+    break;
+    case 'SVD_004':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Begur')";
+    break;
+    case 'SVD_005':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'JP Nagar')";
+    break;
+    case 'SVD_006':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Dodda Kammanahalli)";
+    break;
+    case 'SVD_007':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'Jaya Nagar')";
+    break;
+    case 'SVD_008':
+    $sql="select * from litre WHERE date ='$today' AND delivery_status_e='N' AND active_e='0' AND orderid  IN (select id from order_history where location = 'BTM Layout')";
+    break;
     default:
-      # code...
-      break;
+    # code...
+    break;
   }
 
   $result=mysqli_query($db,$sql);
@@ -140,57 +140,57 @@ else
   $final["user"]=array();
 
 
-   while ($row=mysqli_fetch_assoc($result)) {
+  while ($row=mysqli_fetch_assoc($result)) {
 
-  $orderid=$row['orderid'];
-  $sql2="select * from order_history where id= $orderid";
-  $result2=mysqli_query($db,$sql2);
+    $orderid=$row['orderid'];
+    $sql2="select * from order_history where id= $orderid";
+    $result2=mysqli_query($db,$sql2);
 
-  while ($row2=mysqli_fetch_assoc($result2)) {
-  $address= $row2['address'];
-  }
+    while ($row2=mysqli_fetch_assoc($result2)) {
+      $address= $row2['address'];
+    }
 
     $id= $row['userid'];
-     $sql1="select * from user where id= $id ";
+    $sql1="select * from user where id= $id ";
     $result1=mysqli_query($db,$sql1);
     if ($row['evening_litre']!='0') {
 
       while($row1=mysqli_fetch_assoc($result1)){
-         $user=array(
-           "userid" => $row['userid'],
-           "name"=>$row1['name'],
-           "mobile"=>$row1['mobile'],
-           "orderid" => $row['orderid'],
-           "dateid" => $row['id'],
-           "morning_litre" => $row['evening_litre'],
-           "address"=>$address
-         );
-         array_push($final["user"], $user);
+        $user=array(
+          "userid" => $row['userid'],
+          "name"=>$row1['name'],
+          "mobile"=>$row1['mobile'],
+          "orderid" => $row['orderid'],
+          "dateid" => $row['id'],
+          "morning_litre" => $row['evening_litre'],
+          "address"=>$address
+        );
+        array_push($final["user"], $user);
       }
 
     }
 
 
 
-   }
+  }
 }
 
 
 /*switch ($DBOYID) {
-  case 'SVD_001':
-    $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND orderid  IN (select id from order_history where location = 'Banshankari')";
-    break;
+case 'SVD_001':
+$sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND orderid  IN (select id from order_history where location = 'Banshankari')";
+break;
 
-    case 'SVD_002':
-      $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND orderid  IN (select id from order_history where location = 'Koramangala')";
-      break;
+case 'SVD_002':
+$sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND orderid  IN (select id from order_history where location = 'Koramangala')";
+break;
 
-      case 'SVD_003':
-        $sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND orderid  IN (select id from order_history where location = 'HSRLayout')";
-        break;
-  default:
-    # code...
-    break;
+case 'SVD_003':
+$sql="select * from litre WHERE date ='$today' AND delivery_status_m='N' AND orderid  IN (select id from order_history where location = 'HSRLayout')";
+break;
+default:
+# code...
+break;
 }
 
 $result=mysqli_query($db,$sql);
@@ -205,7 +205,7 @@ $final=array();
 $final["user"]=array();
 
 
- while ($row=mysqli_fetch_assoc($result)) {
+while ($row=mysqli_fetch_assoc($result)) {
 
 $orderid=$row['orderid'];
 $sql2="select * from order_history where id= $orderid";
@@ -215,29 +215,29 @@ while ($row2=mysqli_fetch_assoc($result2)) {
 $address= $row2['address'];
 }
 
-  $id= $row['userid'];
-   $sql1="select * from user where id= $id ";
-  $result1=mysqli_query($db,$sql1);
-  while($row1=mysqli_fetch_assoc($result1)){
-     $user=array(
-       "userid" => $row['userid'],
-       "name"=>$row1['name'],
-       "mobile"=>$row1['mobile'],
-       "orderid" => $row['orderid'],
-       "dateid" => $row['id'],
-       "morning_litre" => $row['morning_litre'],
-       "address"=>$address
-     );
-     array_push($final["user"], $user);
-  }
+$id= $row['userid'];
+$sql1="select * from user where id= $id ";
+$result1=mysqli_query($db,$sql1);
+while($row1=mysqli_fetch_assoc($result1)){
+$user=array(
+"userid" => $row['userid'],
+"name"=>$row1['name'],
+"mobile"=>$row1['mobile'],
+"orderid" => $row['orderid'],
+"dateid" => $row['id'],
+"morning_litre" => $row['morning_litre'],
+"address"=>$address
+);
+array_push($final["user"], $user);
+}
 
 
 }*/
 
 
 
-    $json = json_encode($final["user"]);
-    echo $json;
+$json = json_encode($final["user"]);
+echo $json;
 
 
- ?>
+?>
